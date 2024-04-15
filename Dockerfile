112159c4e7589c0499ecab4396ed558938b06fab
jaksatomovic <jaksa.tomovic@gmail.com>
2024-04-15 10:22:04 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Use the official Python image with Python 3.11
+FROM python:3.11
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Install manually all the missing libraries
+RUN apt-get update && apt-get install -y gcc python3-dev libffi-dev cron
+
+# Copy the requirements.txt file into the container at /app
+COPY requirements.txt .
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy your script into the container at /app
+COPY main.py .
+
+# Copy the cron job file into the container at /etc/cron.d/cronjob
+COPY cronjob /etc/cron.d/cronjob
+
+# Give execution rights on the cron job
+RUN chmod 0644 /etc/cron.d/cronjob
+
+# Apply cron job
+RUN crontab /etc/cron.d/cronjob
+
+# Run the command on container startup
+CMD ["cron", "-f"]
---
+++ main.py
@@ -0,0 +1,28 @@
+import ssl
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+
+smtp_port = 587
+smtp_server = "smtp.gmail.com"
+sender_mail = "jaksa.tomovic@gmail.com"
+sender_pw = "nzgivgfudszoyayz"
+recipients = ["jaksa.tomovic@icloud.com"]
+
+def send_alert(title, message):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"{title}"
+    message["From"] = sender_mail
+    message["To"] = ", ".join(recipients)
+    body = f"{message}"
+    message.attach(MIMEText(body, "html"))
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls(context=context)
+        server.login(sender_mail, sender_pw)
+        server.sendmail(sender_mail, recipients, message.as_string())
+
+if __name__ == "__main__":
+    print(f"Running script at {datetime.now()}")
+    send_alert("This message is sent every 30 minutes", "<h1>Hello World from a Docker container</h1>")
